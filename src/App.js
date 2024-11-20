439fdc950347e3daef66582784aa4416fe11422a
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2024-11-20 23:34:23 +0530

    using bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import AlbumList from "./components/AlbumList";
+import { db } from "./firebaseInit";
 
 function App() {
   const [albums, setAlbums] = useState([])
@@ -9,8 +10,10 @@ function App() {
     getAlbumsFromDb();
   }, []);
 
+
   function getAlbumsFromDb() {
-    
+    console.log(db);
+
   }
   return (
     <>
