9701c5978a5e41a69f3c7b779cf63df5ebe733e2
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2024-11-27 21:37:38 +0530

    toast and loader is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AlbumList from "./components/AlbumList";
-import { db } from "./firebaseInit";
-import { addDoc, collection, doc, getDocs, onSnapshot } from "firebase/firestore";
 
 function App() {
-  const [albums, setAlbums] = useState([])
+  
 
   const router = createBrowserRouter([
     {
@@ -14,35 +12,13 @@ function App() {
       element: (
         <>
           <Navbar />
-          <AlbumList albumSubmitedData={albumSubmitedData} albums={albums} />
+          <AlbumList />
         </>
       )
     }
   ])
 
-  useEffect(() => {
-    getAlbumsFromDb();
-  }, []);
 
-
-  async function getAlbumsFromDb() {
-    let albumsData = await getDocs(collection(db, 'albums'));
-    const newAlbums = [];
-
-    albumsData.forEach((doc) => {
-      newAlbums.push({ id: doc.id, ...doc.data() });
-    });
-
-    setAlbums([...albums, ...newAlbums]); // Spread existing albums and new ones
-  }
-
-
-  async function albumSubmitedData(title) {
-    let docRef = await addDoc(collection(db, 'albums'), { title: title })
-    onSnapshot(doc(db, 'albums', docRef.id), (snapshot) => {
-      setAlbums([...albums, { id: snapshot.id, ...snapshot.data() }])
-    });
-  }
 
 
 
--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,28 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import folderLogo from '../Assets/Images/folder-logo.png'
 import AlbumForm from './AlbumForm'
 import style from '../styles/album.module.css'
 import ImagesList from './ImagesList'
+import { db } from '../firebaseInit'
+import { addDoc, collection, doc, getDocs, onSnapshot } from "firebase/firestore";
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Spinner from 'react-spinner-material'
 
-function AlbumList({ albums, albumSubmitedData }) {
+function AlbumList({ albumSubmitedData }) {
     const [formVisibility, setFormVisibility] = useState(false)
     const [selectedAlbumId, setSelectedAlbumId] = useState(null);
-    const [albumName, setAlbumName] = useState('');
+    const [albumName, setAlbumName] = useState('')
+    const [albums, setAlbums] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     const handleAlbumClick = (albumId, name) => {
         if (albumId) {
             setSelectedAlbumId(albumId);
         }
+
         if (name) {
             setAlbumName(name)
         }
     }
 
+    useEffect(() => {
+        getAlbumsFromDb();
+    }, []);
+
+
+    async function getAlbumsFromDb() {
+        setLoading(true)
+        let albumsData = await getDocs(collection(db, 'albums'));
+        const newAlbums = [];
+
+        albumsData.forEach((doc) => {
+            newAlbums.push({ id: doc.id, ...doc.data() });
+        });
+
+        setAlbums([...albums, ...newAlbums]); // Spread existing albums and new ones
+        setLoading(false)
+        toast.success('Album Loaded Successfullly.', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        })
+    }
+
+
+    async function albumSubmitedData(title) {
+        let docRef = await addDoc(collection(db, 'albums'), { title: title })
+        onSnapshot(doc(db, 'albums', docRef.id), (snapshot) => {
+            setAlbums([...albums, { id: snapshot.id, ...snapshot.data() }])
+        });
+
+        toast.success('Album added Successfullly.', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        })
+    }
     return (
         <>
 
             <Container>
+                <ToastContainer />
                 {selectedAlbumId ?
                     (
                         <Row>
@@ -43,7 +98,11 @@ function AlbumList({ albums, albumSubmitedData }) {
                                     type='button'>{formVisibility ? 'Cancel' : 'Add Album'}
                                 </Button>
                             </Col>
-                            {
+                            {loading ?
+                                <Col md={12} sm={12}>
+                                    <Spinner className='mx-auto' animation='border' />
+                                </Col>
+                                :
                                 albums.map((album) => {
                                     return (
                                         <Col key={album.id} md={3} sm={4} xs={6} onClick={() => handleAlbumClick(album.id, album.title)}>
@@ -54,7 +113,6 @@ function AlbumList({ albums, albumSubmitedData }) {
                                         </Col>
                                     )
                                 })
-
                             }
                         </Row>
                     )
